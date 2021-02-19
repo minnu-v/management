6502835f63fc6f38d0fae2bfce4100ae588fcce9
akhila <akhila.sulthana@radicalinfotech.com>
2021-02-19 17:07:26 +0530

    upload multiple file

--- src/components/employeeManagement/addEmployee/Uploads.js
+++ src/components/employeeManagement/addEmployee/Uploads.js
@@ -1,36 +1,75 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import React, { useState } from "react";
+import Fab from "@material-ui/core/Fab";
+import BackupIcon from "@material-ui/icons/Backup";
+import { makeStyles } from "@material-ui/core/styles";
+import Dropzone from "react-dropzone";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
+  app: {
+    padding: "10px 20px ",
   },
-  input: {
-    display: 'none',
+  extendedIcon: {
+    marginRight: theme.spacing(1),
   },
+  fileName:{
+    paddingTop: "20px"
+  }
 }));
 
-export default function Uploads() {
+export default function Upload() {
   const classes = useStyles();
+  const [fileNames, setFileNames] = useState([]);
+  const handleDrop = (acceptedFiles) =>
+    setFileNames(acceptedFiles.map((file) => file.name));
 
   return (
-    <div className={classes.root}>
-      <input
-        accept="image/*"
-        className={classes.input}
-        id="contained-button-file"
-        multiple
-        type="file"
-      />
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" color="primary" component="span" startIcon={<CloudUploadIcon />}>
-          Uploads
-        </Button>
-      </label>
+    <div className={classes.app}>
+      <Dropzone
+        className={classes.drop}
+        onDrop={handleDrop}
+        
+        minSize={1024}
+        maxSize={3072000}
+      >
+        {({
+          getRootProps,
+          getInputProps,
+          isDragAccept,
+          isDragReject,
+        }) => {
+          const additionalClass = isDragAccept
+            ? "accept"
+            : isDragReject
+            ? "reject"
+            : "";
+
+          return (
+            <div
+              {...getRootProps({
+                className: `dropzone ${additionalClass}`,
+              })}
+            >
+              <input {...getInputProps()} />
+              <Fab 
+              className={classes.fabIcons}
+              variant="extended"
+               size="small"
+      color="primary">
+        <BackupIcon className={classes.extendedIcon} />
+        upload
+      </Fab>
+            </div>
+          );
+        }}
+      </Dropzone>
+      <div className={classes.fileName}>
+        <strong>Files:</strong>
+        <ul className={classes.alignment}>
+          {fileNames.map((fileName) => (
+            <li key={fileName}>{fileName}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
